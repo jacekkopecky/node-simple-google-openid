3d06fd6104cbe725cb7e194a45ef989d18c0cfd0
Jacek Kopecky <jacek@jacek.cz>
2017-12-01 15:27:42 +0000

    add example, esp. for testing

---
+++ example/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+// const GoogleAuth = require('simple-google-openid');
+const GoogleAuth = require('./..');
+
+// you can put your client ID here
+app.use(GoogleAuth(process.env.GOOGLE_CLIENT_ID));
+
+// return 'Not authorized' if we don't have a user
+app.use('/api', GoogleAuth.guardMiddleware({ realm: 'jwt' }));
+
+app.get('/api/hello', (req, res) => {
+  if (req.user.displayName) {
+    res.send('Hello ' + req.user.displayName + '!');
+  } else {
+    res.send('Hello user without a name!');
+  }
+
+  console.log('successful authenticated request by ' + req.user.emails[0].value);
+});
+
+// this will serve the HTML file shown below
+app.use(express.static('static'));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
